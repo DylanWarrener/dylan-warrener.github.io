81760ce3c15840a4d15ac0d05a63919fa8c7a8ae
Dylan Warrener <dqyqlqaqn@gmail.com>
2025-05-17 18:11:16 +0100

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,71 @@
+import vuetify from 'vite-plugin-vuetify';
+import Checker from 'vite-plugin-checker';
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  srcDir: 'src/',
+  alias: {
+    '@/abstractions': './src/abstractions',
+    '@/assets': './src/assets',
+    '@/components': './src/components',
+    '@/layouts': './src/layouts',
+    '@/middleware': './src/middleware',
+    '@/pages': './src/pages',
+    '@/plugins': './src/plugins',
+    '@/server': './src/server',
+    '@/stores': './src/stores',
+    '@/utils': './src/utils',
+  },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    },
+    baseURL: '/dylan-warrener.github.io/', 
+  },
+  plugins: ['~/plugins/vuetify'],
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    },
+  ],
+  modules: ['@pinia/nuxt'],
+  css: ['vuetify/styles', '@/assets/styles/main.scss'],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false, // Required for Vuetify
+    },
+    plugins: [
+      vuetify({
+        autoImport: true,
+      }),
+      Checker({
+        vueTsc: true, // Enables Vue TypeScript checking
+      }),
+    ],
+  },
+  typescript: {
+    strict: true,
+    typeCheck: true, // Enables type-checking at build/development time.
+  },
+  imports: {
+    autoImport: true,
+  },
+  runtimeConfig: {
+    public: {},
+  },
+  compatibilityDate: '2024-11-01',
+  devtools: {
+    enabled: true,
+    timeline: {
+      enabled: true,
+    },
+  },
+  nitro: {
+    preset: 'static', // Ensure static site generation
+  }
+})
