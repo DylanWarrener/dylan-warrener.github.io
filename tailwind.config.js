f129b64f1aee0c18b1b1a32d244cb1d1f317c3cb
Dylan Warrener <dqyqlqaqn@gmail.com>
2025-06-07 15:26:17 +0100

    aded tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class", // Only applies when you manually add the 'dark' class
+  content: ["./src/**/*.{vue,js,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
