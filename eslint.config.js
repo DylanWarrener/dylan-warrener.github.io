8b3c7b8a7121f802a8a6d56e1cb4c371953dd983
Dylan Warrener <dqyqlqaqn@gmail.com>
2025-07-02 15:39:35 +0100

    remove obsolete eslintignore

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,9 @@ import js from "@eslint/js"
 
 export default defineConfig([
   {
+    ignores: ["node_modules/**", ".nuxt/**", "dist/**", "docs/**"],
+  },
+  {
     files: ["src/**/*.js"],
     plugins: {
       js,
