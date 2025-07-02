ab7bd27fd17988b9c5e0a21945a2919162e63b26
Dylan Warrener <dqyqlqaqn@gmail.com>
2025-07-02 16:59:55 +0100

    fix: add nodemailer types

--- src/server/api/contact.post.ts
+++ src/server/api/contact.post.ts
@@ -23,8 +23,8 @@ export default defineEventHandler(async (event: H3Event) => {
     port: Number(process.env.SMTP_PORT || 465),
     secure: true,
     auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
+      user: process.env.SMTP_USER || "",
+      pass: process.env.SMTP_PASS || "",
     },
   })
 
