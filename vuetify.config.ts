f8dfcc85af510f7af8f1a26671eff6520d580068
Dylan Warrener <dqyqlqaqn@gmail.com>
2026-05-02 13:15:54 +0100

    Added colours to vuetify config

--- vuetify.config.ts
+++ vuetify.config.ts
@@ -37,27 +37,36 @@ const lightTheme = {
 const darkTheme = {
   dark: true,
   colors: {
+    /* Background colours from Adobe Colour Pallete - Monochromatic */
+    // These should be used for different elevations / intensities of the same base.
+    /*
+      #183440  ← very dark
+      #395B6A
+      #698895
+      #A6B8BF
+      #B2D9EA  ← light
+     */
+
     // Base UI
-    background: '', // page background
-    layer: '', // cards / nav / panels
-    layerRaised: '', // elevated cards / hover layers
+    background: '#183440', // page background
+    layer: '#395B6A', // cards / nav / panels / sections (slight contrast from background)
+    layerRaised: '#698895', // elevated UI / hover layers / overlays
+    separation: '#698895', // borders / dividers (can be a slight variation of layerRaised)
+
+    // Text system
+    contrast: '#EAF6FF', // primary text / high contrast elements
+    muted: '#A6B8BF', // secondary text / labels / descriptions
+    subtle: '#B2D9EA', // tertiary / icons
 
     // Accent system
-    attention: '#40C4FF',
+    attention: '#40C4FF', // cta buttons / links / highlights
     attentionHover: '',
     attentionActive: '',
     attentionSoft: '',
 
     // States
-    success: '',
-    failed: '',
-
-    // Text
-    contrast: '',
-    muted: '',
-
-    // Structure
-    separation: '',
+    success: 'green',
+    failed: 'red',
   },
 }
 
